3f09f53d8fd90bc5ef3fe50d94c217417d60e9cc
mapfeiff <mapfeiff@uci.edu>
2023-06-07 16:51:55 -0700

    added capability to save subject information to a separate csv

--- PythonFiles/main.py
+++ PythonFiles/main.py
@@ -35,24 +35,24 @@ def main():
     else:
         #First, decide if the subject is an the experimenter, AI, or human (rule: the experimenter uses ID 0 and AI is given a negative ID which is then converted to a positive ID)
         #Experimenter ID given
-        if(subject_information_object.get_ID() < 0):
+        if(subject_information_object.get_ID() == 0):
             #The subject is the experimenter, saved data should only be used for system testing
-            csv_filename = "toy_test_data"
+            csv_filename = "toy"
         #AI ID given
         elif(subject_information_object.get_ID() < 0):
             #The subject is AI, data will go to the AI specific csv
-            csv_filename = "ai_test_data"
+            csv_filename = "ai"
             #We will also convert the ID to a positive number
             id = subject_information_object.get_ID()
             subject_information_object.set_ID(-id)
         #Human ID given
         else:
             #The subject is natural (human), data will go into the human specific csv
-            csv_filename = "human_test_data"
+            csv_filename = "human"
 
         #First open the file with all the previously gathered data
         try:
-            dataframe_Tau = pd.read_csv(f"../{csv_filename}_Tau.csv", header=0)
+            dataframe_Tau = pd.read_csv(f"../{csv_filename}_test_data_Tau.csv", header=0)
         #If the file does not exist, create a new data file
         except:
             header_Tau = ["person", "game", "trial", "decision", "reward", "state"]
@@ -72,10 +72,27 @@ def main():
         #Concatinate all the new dataframes into a single instance
         dataframe_Tau = pd.concat(df_Tau_list)
         #Write the data to the Tau model csv
-        dataframe_Tau.to_csv(f"../{csv_filename}_Tau.csv", index=False)
+        dataframe_Tau.to_csv(f"../{csv_filename}_test_data_Tau.csv", index=False)
         #Lastly, drop the "state" column and add the data to the Win-Stay;Lose-Shift (WSLS) model csv
         dataframe_WSLS = dataframe_Tau.drop(columns=["state"])
-        dataframe_WSLS.to_csv(f"../{csv_filename}_WSLS.csv", index=False)
+        dataframe_WSLS.to_csv(f"../{csv_filename}_test_data_WSLS.csv", index=False)
+
+        #At this point, the subject data has been saved, so save the subject information as well
+        #First open the file with all the previously gathered data
+        try:
+            dataframe_subject = pd.read_csv(f"../{csv_filename}_subject_info.csv", header=0)
+        #If the file does not exist, create a new data file
+        except:
+            header_subject = ["id", "age", "sex"]
+            dataframe_subject = pd.DataFrame(dict(), columns=header_subject)
+        #Then create a dataframe of the new subject information
+        dataframe_new_subject = pd.DataFrame(subject_information_object.get_all_info())
+        #Next, concatinate all the new dataframes to the old one
+        df_subject_list = [dataframe_subject, dataframe_new_subject]
+        dataframe_Tau = pd.concat(df_subject_list)
+        #Lastly, write the dataframe into the csv
+        dataframe_Tau.to_csv(f"../{csv_filename}_subject_info.csv", index=False)
+
 
 #Run the main program when executed as the main file
 if(__name__ == "__main__"):
