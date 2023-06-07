0334f0844ae6c9649357d053810d8055dcb2c66a
mapfeiff <mapfeiff@uci.edu>
2023-06-07 03:16:19 -0700

    finished updating main python file to account for different formats in WSLS and Tau model data files

--- PythonFiles/main.py
+++ PythonFiles/main.py
@@ -35,43 +35,45 @@ def main():
         #Experimenter ID given
         if(subject_information_object.get_ID() < 0):
             #The subject is the experimenter, saved data should only be used for system testing
-            csv_filename = "ai_test_data.csv"
+            csv_filename = "toy_test_data"
         #AI ID given
         elif(subject_information_object.get_ID() < 0):
             #The subject is AI, data will go to the AI specific csv
-            csv_filename = "ai_test_data.csv"
+            csv_filename = "ai_test_data"
             #We will also convert the ID to a positive number
             id = subject_information_object.get_ID()
             subject_information_object.set_ID(-id)
         #Human ID given
         else:
             #The subject is natural (human), data will go into the human specific csv
-            csv_filename = "human_test_data.csv"
+            csv_filename = "human_test_data"
 
-        #First open the file ith all the previously gathered data
+        #First open the file with all the previously gathered data
         try:
-            dataframe = pd.read_csv(f"../{csv_filename}", header=0)
+            dataframe_Tau = pd.read_csv(f"../{csv_filename}_Tau.csv", header=0)
         #If the file does not exist, create a new data file
         except:
-            header = ["person", "game", "trial", "decision", "reward"]
-            dataframe = pd.DataFrame(dict(), columns=header)
+            header_Tau = ["person", "game", "trial", "decision", "reward", "state"]
+            dataframe_Tau = pd.DataFrame(dict(), columns=header_Tau)
         
-        #Create a list of dataframes add additional trials to
-        df_list = list()
-        #append the dataframe with all the previous data to the list
-        df_list.append(dataframe)
+        #Create a list of dataframes to add additional trials to
+        df_Tau_list = list()
+        #append the previos data to the list
+        df_Tau_list.append(dataframe_Tau)
         #Loop through each condition
         condition_list = condition_order_object.get_condition_list()
         for i in range(len(condition_list)):
             #Get the results of the condition and append them to the dataframe list
-            dataframe = experiment.run_test(subject_ID=subject_information_object.get_ID(), test_number=i+1, condition=condition_list[i])
+            dataframe_Tau = experiment.run_test(subject_ID=subject_information_object.get_ID(), test_number=i+1, condition=condition_list[i])
             #append dataframe into list
-            df_list.append(dataframe)
-        #Concatinate all the dataframes into a single instance
-        dataframe = pd.concat(df_list)
-        #Write the data to the csv
-        dataframe.to_csv(f"../{csv_filename}", index=False)
-
+            df_Tau_list.append(dataframe_Tau)
+        #Concatinate all the new dataframes into a single instance
+        dataframe_Tau = pd.concat(df_Tau_list)
+        #Write the data to the Tau model csv
+        dataframe_Tau.to_csv(f"../{csv_filename}_Tau.csv", index=False)
+        #Lastly, drop the "state" column and add the data to the Win-Stay;Lose-Shift (WSLS) model csv
+        dataframe_WSLS = dataframe_Tau.drop(columns=["state"])
+        dataframe_WSLS.to_csv(f"../{csv_filename}_WSLS.csv", index=False)
 
 #Run the main program when executed as the main file
 if(__name__ == "__main__"):
